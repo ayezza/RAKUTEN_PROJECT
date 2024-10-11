b2ab4b3be68101a98a70b6b6a762fc941c0fb0e9
Abdel YEZZA <ayezza@free.fr>
2024-10-11 16:01:12 +0200

    streamlit

--- pages/5_5.Résultats obtenus.py
+++ pages/5_5.Résultats obtenus.py
@@ -3,10 +3,12 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 import pandas as pd
+import requests
 
 
 # This is relative root directory af trai and test images
-IMAGES_ROOT = os.path.join(os.getcwd(), "images")
+#IMAGES_ROOT = os.path.join(os.getcwd(), "images")
+IMAGES_ROOT = r"https://www.anigraphics.fr/images"
 
 
 st.title("Résultats obtenus")
@@ -26,10 +28,12 @@ with tab1:
     col2.metric("Logistic Regression ", "Accuracy", "80%", "normal")
     col2.metric("Logistic Regression ", "f1_score", "79%", "normal")
     col2.write("Le **Logistic Regression** fournit de très bonnes performances.")
-    img_lr = Image.open(IMAGES_ROOT + "/log-reg-lear_curve.png")
+    #img_lr = Image.open(IMAGES_ROOT + "/log-reg-lear_curve.png")
+    img_lr = Image.open(requests.get(IMAGES_ROOT +  "/"  + "log-reg-lear_curve.png", stream=True).raw)
     col3.text("Logistic Regression-Courbes des apprentissages")
     col3.image(img_lr)
-    img_lr = Image.open(IMAGES_ROOT + "/log-reg-cm.png")
+    #img_lr = Image.open(IMAGES_ROOT + "/log-reg-cm.png")
+    img_lr = Image.open(requests.get(IMAGES_ROOT +  "/"  + "llog-reg-cm.png", stream=True).raw)
     col3.text("Logistic Regression-Matrice de Confusion")
     col3.image(img_lr)
     
@@ -42,7 +46,8 @@ with tab1:
     col2.metric("XGBOOST", "Accuracy", "78%", "normal")
     col2.metric("XGBOOST", "f1_score", "78%", "normal")
     col2.write("Le **XGBOOST** fournit des bons résultats presque au même niveau que le Logistic regession.")
-    img_lr = Image.open(IMAGES_ROOT + "/xgboost-lear_curve.png")
+    #img_lr = Image.open(IMAGES_ROOT + "/xgboost-lear_curve.png")
+    img_lr = Image.open(requests.get(IMAGES_ROOT +  "/"  + "xgboost-lear_curve.png", stream=True).raw)
     col3.text("XGBOOST-Courbes des apprentissages")
     col3.image(img_lr)
     
@@ -53,13 +58,16 @@ with tab1:
     col1, col2 = st.columns(2)
     col1.metric("LSTM combiné avec Conv1D", "Accuracy", "79%", "normal")
     col1.metric("LSTM combiné avec Conv1D", "f1_score", "79%", "normal")
-    img = Image.open(IMAGES_ROOT + "/lstm-evaluation.png")
+    #img = Image.open(IMAGES_ROOT + "/lstm-evaluation.png")
+    img = Image.open(requests.get(IMAGES_ROOT +  "/"  + "lstm-evaluation.png", stream=True).raw)
     col1.text("Evaluation du modèle sur l'ensemble de test")
     col1.image(img)
-    img = Image.open(IMAGES_ROOT + "/lstm-conv1d-accu_loss.png")
+    #img = Image.open(IMAGES_ROOT + "/lstm-conv1d-accu_loss.png")
+    img = Image.open(requests.get(IMAGES_ROOT +  "/"  + "lstm-conv1d-accu_loss.png", stream=True).raw)
     col2.text("Evolution de l'accuracy et de la perte")
     col2.image(img)
-    img = Image.open(IMAGES_ROOT + "/lstm-conv1d-cm.png")
+    #img = Image.open(IMAGES_ROOT + "/lstm-conv1d-cm.png")
+    img = Image.open(requests.get(IMAGES_ROOT +  "/"  + "lstm-conv1d-cm.png", stream=True).raw)
     col2.text("LSTM combiné avec Conv1D-Matrice de confusion")
     col2.image(img)
     
@@ -70,10 +78,12 @@ with tab1:
     col1.metric("LSTM renforcé avec GLOVE", "f1_score", "80%", "normal")
     col1.write("GLOVE alié à LSTM semble améliorer les performances. \
         Le fait qu'il comprend plus de mots en anglais qu'en français n'a pa eu l'ffect escompté.")
-    img = Image.open(IMAGES_ROOT + "/lstm-glove-accu_loss.png")
+    #img = Image.open(IMAGES_ROOT + "/lstm-glove-accu_loss.png")
+    img = Image.open(requests.get(IMAGES_ROOT +  "/"  + "lstm-glove-accu_loss.png", stream=True).raw)
     col2.text("Evolution de l'accuracy et de la perte")
     col2.image(img)
-    img = Image.open(IMAGES_ROOT + "/lstm-glove-cm.png")
+    #img = Image.open(IMAGES_ROOT + "/lstm-glove-cm.png")
+    img = Image.open(requests.get(IMAGES_ROOT +  "/"  + "lstm-glove-cm.png", stream=True).raw)
     col2.text("LSTM combiné avec Conv1D - Matrice de confusion")
     col2.image(img)
     
@@ -82,10 +92,12 @@ with tab1:
     col1, col2 = st.columns(2)
     col1.metric("CNN (Conv1D)", "Accuracy", "80%", "normal")  
     col1.metric("CNN (Conv1D)", "f1_score", "80%", "normal")  
-    img = Image.open(IMAGES_ROOT + "/conv1d-accu_loss.png")
+    #img = Image.open(IMAGES_ROOT + "/conv1d-accu_loss.png")
+    img = Image.open(requests.get(IMAGES_ROOT +  "/"  + "conv1d-accu_loss", stream=True).raw)
     col2.text("Evolution de l'accuracy et de la perte")
     col2.image(img)
-    img = Image.open(IMAGES_ROOT + "/conv1d-mc.png")
+    #img = Image.open(IMAGES_ROOT + "/conv1d-mc.png")
+    img = Image.open(requests.get(IMAGES_ROOT +  "/"  + "conv1d-mc", stream=True).raw)
     col2.text("Conv1D - Matrice de confusion")
     col2.image(img)
     
@@ -122,9 +134,11 @@ with tab2:
     col1.metric("PCA et Random Forest", "Accuracy", "+49%", delta_color= "inverse")
     col1.metric("PCA et Random Forest", "f1_score (moy)", "+56%", delta_color= "inverse")
     col1.text("Rapport de classification")
-    img = Image.open(IMAGES_ROOT + "/pca+RF_classif_report.png")
+    #img = Image.open(IMAGES_ROOT + "/pca+RF_classif_report.png")
+    img = Image.open(requests.get(IMAGES_ROOT +  "/"  + "pca+RF_classif_report", stream=True).raw)
     col1.image(img)
-    img = Image.open(IMAGES_ROOT + "/PCA+RF_confusion_matrix.png")
+    #img = Image.open(IMAGES_ROOT + "/PCA+RF_confusion_matrix.png")
+    img = Image.open(requests.get(IMAGES_ROOT +  "/"  + "PCA+RF_confusion_matrix", stream=True).raw)
     col2.text("Matrice de confusion")
     col2.image(img)
     st.write(">La réduction **PCA** appliquée aux images nous a permis de conserver **90%** de la variance expliquée des images en \
@@ -142,7 +156,8 @@ with tab2:
     col1.metric("RESNET 50", "Accuracy", "+48%", delta_color= "inverse")
     col1.write(">Le **RESTNET 50** s'est arrêté de progresser au bout de 36 EOCHS à 48% d'accuracy")
     col2.text("RESNET 50 - Tendances de l'accuracy et de la perte")
-    img = Image.open(IMAGES_ROOT + "/acc_loss_resnet.png")
+    #img = Image.open(IMAGES_ROOT + "/acc_loss_resnet.png")
+    img = Image.open(requests.get(IMAGES_ROOT +  "/"  + "acc_loss_resnet", stream=True).raw)
     col2.image(img)
     
     st.html("<hr>")
@@ -152,7 +167,8 @@ with tab2:
     col1.metric("EfficentNet B5", "Accuracy", "+46%", delta_color= "inverse")
     col1.write(">Le **EfficentNet B5** s'est arrêté de progresser au bout de 15 EOCHS à 46% d'accuracy")
     col2.text("EfficentNet B5 - Tendances de l'accuracy et de la perte")
-    img = Image.open(IMAGES_ROOT + "/acc_loss_effnet.png")
+    #img = Image.open(IMAGES_ROOT + "/acc_loss_effnet.png")
+    img = Image.open(requests.get(IMAGES_ROOT +  "/"  + "acc_loss_effnet", stream=True).raw)
     col2.image(img)
     
     st.html("<hr>")
@@ -161,7 +177,8 @@ with tab2:
     col1.metric("ViT (Vision Transformer)", "Accuracy", "+52%", delta_color= "inverse")
     col1.write(">Le **ViT** a bien terminé ses 10 EPOCHs avec 52% d'accuracy")
     col2.text("ViT - Tendances de l'accuracy et de la perte")
-    img = Image.open(IMAGES_ROOT + "/acc_loss_vit.png")
+    #img = Image.open(IMAGES_ROOT + "/acc_loss_vit.png")
+    img = Image.open(requests.get(IMAGES_ROOT +  "/"  + "acc_loss_vit", stream=True).raw)
     col2.image(img)
     
     st.write(">Les trois modèle offrent un niveau de précision presque équivalent à celui des modèles baseline")
